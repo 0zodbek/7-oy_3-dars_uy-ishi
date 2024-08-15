5c62d473b64bc532c9a5b9b9f360520619107304
0zodbek <ozpdbekov@gmail.com>
2024-08-16 03:09:22 +0500

    sd

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DynamicForm from './components/ynamicForms/DynamicForms'
+function App() {
+  return (
+    <div>
+      <DynamicForm/>
+    </div>
+  )
+}
+
+export default App
